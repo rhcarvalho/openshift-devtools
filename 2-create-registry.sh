be7519362c636c29c2e25242325412a663d9be46
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-06-11 18:13:50 +0200

    Set permissions with registry/project creation
    One less step to execute.

--- 2-create-registry.sh
+++ 2-create-registry.sh
@@ -4,3 +4,4 @@ OPENSHIFT_REGISTRY_CONFIG=`pwd`/openshift.local.config/master/openshift-registry
 [[ -f $OPENSHIFT_REGISTRY_CONFIG ]] || (echo "registry config not found"; exit 1)
 sudo chmod +r $OPENSHIFT_REGISTRY_CONFIG
 oadm registry --create --credentials=$OPENSHIFT_REGISTRY_CONFIG
+oadm policy add-role-to-user view demo -n default
--- 4-set-permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-oadm policy add-role-to-user view demo -n default
-oadm policy add-role-to-user admin demo -n demo
