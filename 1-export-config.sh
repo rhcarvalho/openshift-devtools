a9060032298d166ec2a7dcc53deb2f33fde4d9c7
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-06-12 16:58:25 +0200

    Update config env var name

--- 1-export-config.sh
+++ 1-export-config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # source this file to export the variables below:
-export OPENSHIFTCONFIG=`pwd`/openshift.local.config/master/admin.kubeconfig
-[[ -f $OPENSHIFTCONFIG ]] || (echo "OpenShift config not found"; exit 1)
+export KUBECONFIG=`pwd`/openshift.local.config/master/admin.kubeconfig
+[[ -f $KUBECONFIG ]] || (echo "OpenShift config not found"; exit 1)
 export CURL_CA_BUNDLE=`pwd`/openshift.local.config/master/ca.crt
 [[ -f $CURL_CA_BUNDLE ]] || (echo "certificate not found"; exit 1)
 
 (
   set -x
-  sudo chmod a+rwX "$OPENSHIFTCONFIG"
+  sudo chmod a+rwX "$KUBECONFIG"
 )
