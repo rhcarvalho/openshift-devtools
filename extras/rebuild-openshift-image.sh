rhcarvalho/openshift-devtools

commit b9b0addc48549970ef02cb36321ab7e52a910c8d

Tag base image before referencing it
Newer versions of Docker do not allow referencing images by ID in
Dockerfiles.

extras/rebuild-openshift-image.sh
@@ -6,6 +6,7 @@ function rebuild-openshift-image() {
   set -e
 
   image="$1"
+  base_image="${image}-base"
 
   tmpdir="$(mktemp -d)"
   openshiftbin="$(which openshift)"
@@ -13,15 +14,18 @@ function rebuild-openshift-image() {
   info "Copying openshift binary to temporary work directory ..."
   cp -av "${openshiftbin}" "${tmpdir}"
 
-  # Find parent image id to avoid stacking multiple COPY layers
-  parent=$(docker history --no-trunc ${image} | \
-           grep -v 'COPY file.*in /usr/bin/openshift' | \
-           awk 'NR==2 {print $1}')
+  # Find base image id to avoid stacking multiple COPY layers
+  base_id=$(docker history --no-trunc ${image} | \
+            grep -v 'COPY file.*in /usr/bin/openshift' | \
+            awk 'NR==2 {print $1}')
+
+  info "Tagging the base Docker image ${base_image} ..."
+  docker tag "${base_id}" "${base_image}"
 
   # Write Dockerfile
   set +e
   read -d '' Dockerfile <<EOF
-  FROM ${parent}
+  FROM ${base_image}
 
   COPY openshift /usr/bin/openshift
 EOF

commit f0f014bfbcd1d6f4e4ecab561fe0f241c2339044

Generate random base image tag and untag when done
Fixes a bug introduced in b9b0add: running the script for a second time
would fail because we try to retag an image.

extras/rebuild-openshift-image.sh
@@ -6,7 +6,7 @@ function rebuild-openshift-image() {
   set -e
 
   image="$1"
-  base_image="${image}-base"
+  base_image="${image}-base-$(tr -dc '[:lower:][:digit:]' </dev/urandom | dd bs=4 count=3 2>/dev/null)"
 
   tmpdir="$(mktemp -d)"
   openshiftbin="$(which openshift)"
@@ -37,6 +37,7 @@ EOF
 
   # Cleanup
   rm -vrf "${tmpdir}"
+  docker rmi "${base_image}"
 
   info "Done."
 }